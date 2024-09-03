2406bfd2d1aee2d3fbee335ea7b5457195cbae54
Abhishadas <abhishadas9004@gmail.com>
2024-09-03 16:41:42 +0530

    commit

--- students/students/settings.py
+++ students/students/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-v7q7kis$tf64e1i+(a(6(=qej^kq3x$t8(ns!g*y&by_9*n_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'student_test',
-        'HOST': 'localhost',
-        'PORT': '5432',
         'USER': 'postgres',
         'PASSWORD': 'abhi9004',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
